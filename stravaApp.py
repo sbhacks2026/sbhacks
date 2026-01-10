ded38dc53c1127adeb64c3ecb8df8ee46c799399
holdenellismain <holdenellis66@gmail.com>
2026-01-10 13:46:31 -0800

    update Activity

--- stravaApp.py
+++ stravaApp.py
@@ -21,20 +21,23 @@ class StravaApp:
     def __init__(self, token : str):
         self.token = token
 
+    def get_walking_activities(self):
+        request_url = f'https://www.strava.com/api/v3/athlete/activities'
+        header = {"Authorization" : "Bearer " + self.token}
 
-def get_walking_activities(access_token):
-    request_url = f'https://www.strava.com/api/v3/athlete/activities'
-    header = {"Authorization" : "Bearer " + access_token}
+        result = requests.get(request_url, headers=header).json()
 
-    result = requests.get(request_url, headers=header).json()
+        target_sports = {"Run", "Hike", "TrailRun"} # filter to only backpacking related things
 
-    target_sports = {"Run", "Hike", "TrailRun"} # filter to only backpacking related things
+        filtered_activities = [
+            Activity.Activity(activity) for activity in result
+            if activity.get("sport_type") in target_sports
+        ]
 
-    filtered_activities = [
-        activity for activity in result
-        if activity.get("sport_type") in target_sports
-    ]
+        return Activity.ActivityContainer(filtered_activities)
     
-    return filtered_activities
-
-print(get_walking_activities(token))
+    
+app = StravaApp(token)
+activities = app.get_walking_activities()
+print(activities)
+activities.to_json('strava_data.json')
