581fa6f373450cb92c162b8496caddaf123e6715
holdenellismain <holdenellis66@gmail.com>
2026-01-10 12:45:20 -0800

    updated StravaApp

--- stravaApp.py
+++ stravaApp.py
@@ -1,27 +1,13 @@
 import json
 import requests
-
-def get_keys(path='config.json'):
-    try:
-        with open(path, 'r') as file:
-            config = json.load(file)
-        return config
-    except FileNotFoundError:
-        print(f"Configuration file {path} not found.")
-        return None
-
-config = get_keys()
-id = config.get('CLIENT_ID')
-secret = config.get('CLIENT_SECRET')
-referesh_token = config.get('CODE')
-
+import Activity 
 def get_token():
     auth_url = "https://www.strava.com/oauth/token"
     data = {
-        'client_id' : id,
-        'client_secret' : secret,
+        'client_id' : "131194",
+        'client_secret' : "7887ba703fc006c6005393540d018817b8a2151c",
         'grant_type' : 'refresh_token',
-        'refresh_token' : referesh_token
+        'refresh_token' : '743893e4a18f830aa6b837a1b7492b766d54f0a7'
     }
 
     #request
@@ -29,14 +15,26 @@ def get_token():
     access_token = auth_result['access_token']
     return access_token
 
-def get_segment_stats(id, access_token):
-    request_url = f'https://www.strava.com/api/v3/segments/{id}'
+token = get_token()
+
+class StravaApp:
+    def __init__(self, token : str):
+        self.token = token
+
+
+def get_walking_activities(access_token):
+    request_url = f'https://www.strava.com/api/v3/athlete/activities'
     header = {"Authorization" : "Bearer " + access_token}
 
     result = requests.get(request_url, headers=header).json()
+
+    target_sports = {"Run", "Hike", "TrailRun"} # filter to only backpacking related things
+
+    filtered_activities = [
+        activity for activity in result
+        if activity.get("sport_type") in target_sports
+    ]
     
-    try:
-        return [result['effort_count'], result['athlete_count']]
-    except:
-        print('Error with id:', id)
-        return [0, 0]
+    return filtered_activities
+
+print(get_walking_activities(token))
