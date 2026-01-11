51968b9225beb11e2f2ea4cb238e00ae9f4f365b
kbarker223 <kaiandreasbarker@gmail.com>
2026-01-10 17:39:13 -0800

    return

--- OAuth/stravaApp.py
+++ OAuth/stravaApp.py
@@ -20,11 +20,11 @@ class StravaApp:
         target_sports = {"Run", "Hike", "TrailRun"} # filter to only backpacking related things
 
         filtered_activities = [
-            Activity.Activity(activity) for activity in result
+            Activity.Activity(activity) for activity in result[1]
             if activity.get("sport_type") in target_sports
         ]
 
-        return Activity.ActivityContainer(filtered_activities)
+        return Activity.ActivityContainer(filtered_activities[0])
     
 token = sys.argv[1] if len(sys.argv) > 1 else None
 app = StravaApp(token)
