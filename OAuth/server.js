42aaa83a4de31f68e1be4911207b2055e5ae6b42
Tony Segal <tony-segal@outlook.com>
2026-01-11 13:40:18 -0800

    Revert "Update server"
    This reverts commit d6cde8f1c0927654eda5a9a7554db14bfad08677.

--- OAuth/server.js
+++ OAuth/server.js
@@ -343,7 +343,8 @@ app.get('/api/recommendation', async (req, res) => {
                     // Call search_trail.py to find the AllTrails link
                     const searchPython = spawn('python3', [
                         'search_trail.py',
-                        trailName
+                        trailName,
+                        preferences.city || ''
                     ]);
 
                     let searchResult = '';
